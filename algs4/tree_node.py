8ef23861dfa226e099f8d7b2488d5baf1a829e37
septimochen <qizu.chen@hotmail.com>
2020-12-14 22:43:15 +0800

    back traverse

--- algs4/tree_node.py
+++ algs4/tree_node.py
@@ -10,12 +10,17 @@ def traverse(t):
         return
     else:
         traverse(t.left)
-        print(t.val)
         traverse(t.right)
+        print(t.val)
 
 
 if __name__ == "__main__":
     tree = TreeNode(1)
     tree.left = TreeNode(2)
     tree.right = TreeNode(3)
+    # traverse(tree)
+    tree.left.left = TreeNode(4)
+    tree.left.right = TreeNode(7)
+    tree.right.right = TreeNode(5)
+    tree.right.left = TreeNode(8)
     traverse(tree)
